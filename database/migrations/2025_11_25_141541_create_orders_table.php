b7e7932e2e2945b8bd20d079627240041a250348
SatriaPutra4 <satriaputraardyb@gmail.com>
2025-11-25 21:27:21 +0700

    update untuk file migrasi

--- database/migrations/2025_11_25_141541_create_orders_table.php
+++ database/migrations/2025_11_25_141541_create_orders_table.php
@@ -6,20 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->decimal('total_price', 10, 2);
+            $table->text('address');
+            $table->enum('payment_status', ['pending', 'paid'])->default('pending');
+            $table->enum('order_status', ['pending', 'processing', 'shipped', 'completed'])->default('pending');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('orders');
