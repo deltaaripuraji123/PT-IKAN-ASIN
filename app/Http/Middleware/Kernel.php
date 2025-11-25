b71ffc0ee891615ed583ab3a338b64e0ff1609d0
SatriaPutra4 <satriaputraardyb@gmail.com>
2025-11-25 21:52:48 +0700

    perubahan pada kernel

--- app/Http/Middleware/Kernel.php
+++ app/Http/Middleware/Kernel.php
@@ -1,4 +1,18 @@
-protected $routeMiddleware = [
-    // ... middleware lainnya
-    'admin' => \App\Http\Middleware\IsAdmin::class,
-];
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    protected $routeMiddleware = [
+        // middleware bawaan laravel
+        'auth' => \App\Http\Middleware\Authenticate::class,
+        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+
+        // middleware custom
+        'admin' => \App\Http\Middleware\IsAdmin::class,
+    ];
+}
